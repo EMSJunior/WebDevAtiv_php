1e4c74bc875b1bd339f2900694b94d60c416505c
Edson Junior - Computador de casa <jumendessouza1@gmail.com>
2023-06-13 08:32:05 -0300

    table basic create

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estados Brasileiros</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <table>
+            <tr>
+
+                <th>UF</th>
+                <th>Abv</th>
+                <th>Sede</th>
+                <th>Area (km2)</th>
+                <th>Populacao (2014)</th>
+                <th>Densidade(2005)</th>
+                <th>PIB (2015)</th>
+                <th>(% total) (2015)</th>
+                <th>PIB per capta (R$) (2005)</th>
+                <th>IDH (2010)</th>
+                <th>Alfabetizacao (2016)</th>
+                <th>Mortalidade Infantil</th>
+                <th>Expectativa de Vida</th>
+            </tr>
+        </table>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
